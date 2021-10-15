e2ec842fd04f3895654dea8dd107f3962cc81a34
IonutSebastian <72657528+IonutSebastian@users.noreply.github.com>
2021-10-15 13:34:09 +0300

    Add files via upload

---
+++ IonutSebastian_lab2/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace IonutSebastian_lab2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void txtMargheritaPizza_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void txtPepperoniPizza_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void txtQuantity_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void cmb_Type_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void btnAddToSale_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
